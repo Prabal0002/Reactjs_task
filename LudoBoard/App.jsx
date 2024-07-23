3fed094469b36d4d36ba954ca3ac7b07ca1033db
Prabal Singh Bhadoriya <129841865+Prabal0002@users.noreply.github.com>
2024-07-23 16:44:15 +0530

    Update App.jsx

--- LudoBoard/App.jsx
+++ LudoBoard/App.jsx
@@ -1,1 +1,20 @@
+import React from 'React';
+import './App.css';
+import LudoBoard from './LudoBoard.jsx';
+
+
+function App(){
+  
+    return (
+      <div className="App">
+        <h1>States in react</h1>
+    
+       <LudoBoard/>
+          
+      </div>
+    );
+  }
+
+
+export default App;
 
