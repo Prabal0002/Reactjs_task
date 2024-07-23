b8fe8a11b13a7b049dee7b85060301d7ba8195a8
Prabal Singh Bhadoriya <129841865+Prabal0002@users.noreply.github.com>
2024-07-23 16:37:05 +0530

    Update LikeButton.jsx

--- Like button/LikeButton.jsx
+++ Like button/LikeButton.jsx
@@ -1,1 +1,35 @@
 
+import {useState} from 'react';
+export default function LikeButton(){
+ let [isLiked, setIsliked]=useState(false);
+let[clicks,setClicks] = useState(0);
+
+   let toggleLike=() =>{
+
+    setIsliked(!isLiked);
+    setClicks(clicks+1);
+    
+  };
+   let likeStyle={color:"red"
+};
+ return (
+   <div>
+   <h1>Clicks={clicks}</h1>
+  <p onClick ={toggleLike}>{isLiked? ( <i className="fa-solid fa-heart" style={likeStyle}></i>
+ ):(
+
+     <i className="fa-regular fa-heart"></i>
+     )}
+
+     </p>
+     
+   </div>
+ );
+  
+}
+
+
+
+
+
+
