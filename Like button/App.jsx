f230d052702b93360582591fc013f7c612b0d4db
Prabal Singh Bhadoriya <129841865+Prabal0002@users.noreply.github.com>
2024-07-23 16:39:10 +0530

    Update App.jsx

--- Like button/App.jsx
+++ Like button/App.jsx
@@ -1,1 +1,20 @@
+import React from 'react';
+import './App.css';
+import LikeButton from './LikeButton.jsx';
+
+
+function App(){
+  
+    return (
+      <div className="App">
+        <h1>States in react</h1>
+    
+       <LikeButton />
+          
+      </div>
+    );
+  }
+
+
+export default App;
 
